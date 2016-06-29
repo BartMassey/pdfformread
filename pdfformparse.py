4a325d911fe5f48f2e932bb223f17af4692ffd85
Bart Massey <bart@cs.pdx.edu>
2016-06-28 20:36:29 -0700

    cleaned up form parse exceptions

--- pdfformparse.py
+++ pdfformparse.py
@@ -14,23 +14,26 @@ from pdfminer.pdfparser import PDFParser
 from pdfminer.pdfdocument import PDFDocument
 from pdfminer.pdftypes import resolve1, PDFObjRef
 
-class FieldTypeException(Exception):
+class FormParseException(Exception):
     pass
 
 def load_form(filename):
     """Load pdf form contents into a dictionary"""
     with open(filename, 'rb') as file:
-        parser = PDFParser(file)
-        doc = PDFDocument(parser)
-        parser.set_document(doc)
-        if not 'AcroForm' in doc.catalog:
-            return None
-        fieldlist = [load_field(resolve1(f)) for f in
-                     resolve1(doc.catalog['AcroForm'])['Fields']]
-        fieldset = dict()
-        for k, v in fieldlist:
-            fieldset[k] = v
-        return fieldset
+        try:
+            parser = PDFParser(file)
+            doc = PDFDocument(parser)
+            parser.set_document(doc)
+            if not 'AcroForm' in doc.catalog:
+                return None
+            fieldlist = [load_field(resolve1(f)) for f in
+                         resolve1(doc.catalog['AcroForm'])['Fields']]
+            fieldset = dict()
+            for k, v in fieldlist:
+                fieldset[k] = v
+            return fieldset
+        except UnicodeDecodeError, e:
+            raise FormParseException(filename + ": unicode error: " + str(e))
 
 def load_field(field):
     """load form field"""
@@ -54,4 +57,4 @@ def load_field(field):
     elif typ == "Btn":
         return (t, uniflail(resolve1(field.get('V')).name))
     else:
-        raise FieldTypeException("unknown field type " + typ)
+        raise FormParseException("unknown field type " + typ)
